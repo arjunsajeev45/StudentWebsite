2b6b8145e16cea1688422044cf64f642ddf67212
speehive <speehive@DESKTOP-TPF0JOE>
2022-12-27 16:25:26 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentWebsite.Helper;
+
+namespace StudentWebsite.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetDetails(string name , int cls,int roll)
+        {
+            ViewBag.n = name;
+            ViewBag.c = cls;
+            ViewBag.r = roll;
+
+            HelperClass obj = new HelperClass();
+            obj.WriteaFile(name,cls, roll);
+            return View();
+        }
+
+
+    }
+}
---
+++ Helper/HelperClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace StudentWebsite.Helper
+{
+    public class HelperClass
+    {
+        
+        public void WriteaFile(string name, int cls, int roll)
+        {
+            var path = @"C:\Users\Rinsh\source\repos\StudentWebsite\Note\A.txt";
+             StreamWriter ob = new StreamWriter(path, true);
+
+            ob.WriteLine("\nName      :" + name);
+            ob.WriteLine("\nclass     :" + cls);
+            ob.WriteLine("\nRollNo    :" + roll);
+            
+            ob.Close();
+            Process.Start("notepad.exe", path);
+
+        }
+    }
+}
